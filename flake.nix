bb535c1260d43051a85604a7af604c8583ba62f3
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2023-10-16 19:12:33 +0200

    on replonge dans le bain

--- flake.nix
+++ flake.nix
@@ -18,11 +18,47 @@
       ]; };
     in
     {
+      packages.${system} = {
+        notes = pkgs.writeShellApplication {
+          name = "notes";
+          runtimeInputs = [ pkgs.emacs ];
+          text = ''
+            emacs -q -l ./.init.el notes.org &
+          '';
+        };
+        simgrid = pkgs.simgrid.overrideAttrs (finalAttrs: previousAttrs: {
+            patches = [ ./test.patch ];
+        });
+        ior-simgrid = pkgs.ior.overrideAttrs (finalAttrs: previousAttrs: {
+            pname = "ior-simgrid";
+            propagatedBuildInputs = [ pkgs.simgrid ];
+            configurePhase = ''
+              ./bootstrap && SMPI_PRETEND_CC=1 ./configure --prefix=$out MPICC=${pkgs.simgrid}/bin/smpicc CC=${pkgs.simgrid}/bin/smpicc
+            '';
+          });
+      };
       devShells.${system} = {
         default = pkgs.mkShell {
-          buildInputs = [
+          packages = [
             qorn.ior-simgrid
             myR
+          ];
+        };
+        expe = pkgs.mkShell {
+          packages = [
+            self.packages.${system}.ior-simgrid
+          ];
+          shellHook = ''
+            ln -sf ${self.packages.${system}.ior-simgrid}/bin/ior ior.bin
+          '';
+        };
+        dev = pkgs.mkShell {
+          packages = [
+            # pkgs.simgrid
+            self.packages.${system}.simgrid
+            pkgs.gnumake
+            pkgs.gnat
+            pkgs.pkg-config
           ];
         };
       };
